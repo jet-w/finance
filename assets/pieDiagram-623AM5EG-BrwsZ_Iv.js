852a31122d126d3e2a68049e9dc3b86defc8c02c
jet-w <140158401+jet-w@users.noreply.github.com>
2025-01-06 06:22:55 +0000

    Deploying to gh-pages from @ jet-w/finance@75d8e79c646d5115c47ec3bd85c4c06859fca5f8 🚀

---
+++ assets/pieDiagram-623AM5EG-BrwsZ_Iv.js
@@ -0,0 +1,30 @@
+import{c as X}from"./chunk-4KE642ED-lqrpjj2X.js";import{p as Y}from"./gitGraph-YCYPL57B-QPBUOVDP-B_8PRtQO.js";import{i as j,m as r,v as G,J as y,aW as I,Q as K,j as U,aU as V,aX as Z,aY as L,aZ as q,k as H,L as _,A as ee,w as te,B as ae,F as ie,M as re}from"./mermaid.esm.min-FBwtPeio.js";import"./chunk-BKDDFIKN-XWUR1A_N.js";import"./app-DyVU9W_i.js";var O=j.pie,D={sections:new Map,showData:!1,config:O},u=D.sections,v=D.showData,le=structuredClone(O),se=r(()=>structuredClone(le),"getConfig"),oe=r(()=>{u=new Map,v=D.showData,G()},"clear"),ne=r(({label:e,value:a})=>{u.has(e)||(u.set(e,a),y.debug(`added new section: ${e}, with value: ${a}`))},"addSection"),pe=r(()=>u,"getSections"),de=r(e=>{v=e},"setShowData"),ce=r(()=>v,"getShowData"),R={getConfig:se,clear:oe,setDiagramTitle:_,getDiagramTitle:ee,setAccTitle:te,getAccTitle:ae,setAccDescription:ie,getAccDescription:re,addSection:ne,getSections:pe,setShowData:de,getShowData:ce},fe=r((e,a)=>{X(e,a),a.setShowData(e.showData),e.sections.map(a.addSection)},"populateDb"),ge={parse:r(async e=>{let a=await Y("pie",e);y.debug(a),fe(a,R)},"parse")},me=r(e=>`
+  .pieCircle{
+    stroke: ${e.pieStrokeColor};
+    stroke-width : ${e.pieStrokeWidth};
+    opacity : ${e.pieOpacity};
+  }
+  .pieOuterCircle{
+    stroke: ${e.pieOuterStrokeColor};
+    stroke-width: ${e.pieOuterStrokeWidth};
+    fill: none;
+  }
+  .pieTitleText {
+    text-anchor: middle;
+    font-size: ${e.pieTitleTextSize};
+    fill: ${e.pieTitleTextColor};
+    font-family: ${e.fontFamily};
+  }
+  .slice {
+    font-family: ${e.fontFamily};
+    fill: ${e.pieSectionTextColor};
+    font-size:${e.pieSectionTextSize};
+    // fill: white;
+  }
+  .legend text {
+    fill: ${e.pieLegendTextColor};
+    font-family: ${e.fontFamily};
+    font-size: ${e.pieLegendTextSize};
+  }
+`,"getStyles"),ue=me,he=r(e=>{let a=[...e.entries()].map(l=>({label:l[0],value:l[1]})).sort((l,h)=>h.value-l.value);return I().value(l=>l.value)(a)},"createPieArcs"),xe=r((e,a,l,h)=>{y.debug(`rendering pie chart
+`+e);let d=h.db,T=K(),C=U(d.getConfig(),T.pie),k=40,s=18,c=4,o=450,x=o,S=V(a),n=S.append("g");n.attr("transform","translate("+x/2+","+o/2+")");let{themeVariables:i}=T,[b]=Z(i.pieOuterStrokeWidth);b??(b=2);let A=C.textPosition,f=Math.min(x,o)/2-k,W=L().innerRadius(0).outerRadius(f),B=L().innerRadius(f*A).outerRadius(f*A);n.append("circle").attr("cx",0).attr("cy",0).attr("r",f+b/2).attr("class","pieOuterCircle");let M=d.getSections(),w=he(M),P=[i.pie1,i.pie2,i.pie3,i.pie4,i.pie5,i.pie6,i.pie7,i.pie8,i.pie9,i.pie10,i.pie11,i.pie12],p=q(P);n.selectAll("mySlices").data(w).enter().append("path").attr("d",W).attr("fill",t=>p(t.data.label)).attr("class","pieCircle");let z=0;M.forEach(t=>{z+=t}),n.selectAll("mySlices").data(w).enter().append("text").text(t=>(t.data.value/z*100).toFixed(0)+"%").attr("transform",t=>"translate("+B.centroid(t)+")").style("text-anchor","middle").attr("class","slice"),n.append("text").text(d.getDiagramTitle()).attr("x",0).attr("y",-(o-50)/2).attr("class","pieTitleText");let $=n.selectAll(".legend").data(p.domain()).enter().append("g").attr("class","legend").attr("transform",(t,g)=>{let m=s+c,E=m*p.domain().length/2,J=12*s,N=g*m-E;return"translate("+J+","+N+")"});$.append("rect").attr("width",s).attr("height",s).style("fill",p).style("stroke",p),$.data(w).append("text").attr("x",s+c).attr("y",s-c).text(t=>{let{label:g,value:m}=t.data;return d.getShowData()?`${g} [${m}]`:g});let Q=Math.max(...$.selectAll("text").nodes().map(t=>(t==null?void 0:t.getBoundingClientRect().width)??0)),F=x+k+s+c+Q;S.attr("viewBox",`0 0 ${F} ${o}`),H(S,o,F,C.useMaxWidth)},"draw"),Se={draw:xe},Te={parser:ge,db:R,renderer:Se,styles:ue};export{Te as diagram};
